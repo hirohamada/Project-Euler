d6fc872b4f2e419df012d6472fdf4816a4b51443
hirohamada <hiroaki.hamada@oist.jp>
2015-05-08 03:00:41 +0900

    Edit again Euler001.m

--- Euler001.m
+++ Euler001.m
@@ -4,13 +4,4 @@
 
 function [answer] = Euler001()
 
-	max_num = 1000;
-	answer = 0;
-
-	for i = 1:max_num
-		% add in answer if a number is a multiple of 3 or 5
-		if ((mod(i,3) == 0) | (mod(i,5) == 0))
-			answer = answer + i;
-		end
-	end
 end
